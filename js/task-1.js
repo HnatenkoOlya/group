d67c6fe20de3e982ea2ee9451195bef61e59c95a
HnatenkoOlya <gnatenkoolgaig1210@gmail.com>
2024-12-08 15:27:59 +0100

    3

--- js/task-1.js
+++ js/task-1.js
@@ -3,42 +3,38 @@
 // function getCode (min) {
 //  if (min > 0 && min < 15) {
 //     return "first";
-//  }  
+//  }
 //  if (min >= 15 && min < 30) {
 //     return "second";
-//  }  
+//  }
 //  if (min >= 30 && min < 45) {
 //     return "third";
-//  }  
+//  }
 //  if (min >= 45 && min <= 59) {
 //     return "fourth";
-//  }  
+//  }
 //   return "Incorrect";
 // }
 // console.log(getCode(60))
-
-
-
 
 // Змінна num може набувати 4 значення: 1, 2, 3 або 4. Якщо вона має
 // значення '1', то у змінну result запишемо 'зима', якщо має значення
 // '2' - 'весна' і так далі. Розв'яжіть завдання через switch-case.
 
-
 // function getSeason(num) {
 //    let result
 //    switch (num) {
 //       case 1:
-//          result = "winter";         
+//          result = "winter";
 //          break;
 //       case 2:
-//          result = "spring";         
+//          result = "spring";
 //          break;
 //       case 3:
-//          result = "summer";         
+//          result = "summer";
 //          break;
 //       case 4:
-//          result = "autumn";         
+//          result = "autumn";
 //          break;
 //       default: result = "notFound";
 //          break;
@@ -47,3 +43,18 @@
 // }
 
 // console.log(getSeason(3));
+//~ Створи функцію logNumbers яка приймає min і max
+//~ і виводить у консоль
+//~ числа від max до min за спаданням
+//~ Додайте усі парні числа від min до max і поверніть результат з функції
+//function logNumbers(min, max) {
+  //let sum = 0;
+  //for (let i = max; i >= min; i--) {
+   // if (i % 2 === 0) {
+     //sum += i
+   // }
+   // console.log(i)
+  //}
+ // return sum;
+//}
+//console.log(logNumbers(2, 10));
